7f26eef6146a665c33e40ee517d241ee018ffd7a
Юрий А. Денисов <yudenisov@mail.ru>
2018-09-07 18:59:47 +0400

    Version 2.0.2. Beta
    Change Set of used commands

--- Source/Elevation/skype_bye.bat
+++
@@ -1,6 +0,0 @@
-taskkill /IM Skype.exe /T /F
-taskkill /IM YouCamTray.exe /T /F
-taskkill /IM YCMMirage.exe /T /F
-taskkill /IM YouCamTray.exe /T /F
-taskkill /IM LWS.exe /T /F
-rem net stop audiosrv
